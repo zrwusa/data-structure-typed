13059971a85c3e8466e96e28eb088520541dd8e0
Revone <zrwusa@gmail.com>
2023-08-18 22:51:45 +0800

    Isolated type removed

--- src/data-structures/types/binary-tree.ts
+++ src/data-structures/types/binary-tree.ts
@@ -7,9 +7,3 @@ export type BinaryTreeNodeId = number;
 export type BinaryTreeDeleted<T> = { deleted: BinaryTreeNode<T> | null | undefined, needBalanced: BinaryTreeNode<T> | null };
 export type ResultByProperty<T> = T | BinaryTreeNode<T> | number | BinaryTreeNodeId;
 export type ResultsByProperty<T> = ResultByProperty<T>[];
-
-export interface BinaryTreeNodeObj<T> {
-    id: BinaryTreeNodeId;
-    val: T;
-    count?: number;
-}
