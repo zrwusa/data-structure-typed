dd7b6412730fbc9bb0eda8b11cbe932987511cd0
Kirk Qi <qilinaus@gmail.com>
2023-10-15 22:09:26 +0800

    [heap, priority queue] Heap improved. References #123: redesigned

--- src/types/data-structures/heap.ts
+++ src/types/data-structures/heap.ts
@@ -1,5 +1,1 @@
-export type HeapOptions<T> = {
-  priorityExtractor?: (element: T) => number;
-  // TODO there is an idea that support chaining which is for conveniently using the data structure
-  // isChaining? : boolean
-};
+export type CompareFunction<T> = (a: T, b: T) => number;
