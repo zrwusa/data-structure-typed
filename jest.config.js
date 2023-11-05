919efc04a21c6664147fb0be2f5f7121a6b3bbb3
Revone <zrwusa@gmail.com>
2023-11-05 21:37:01 +0800

    [test] coverage directories specified

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/**/*.test.ts', '<rootDir>/test/**/*.test.js'],
+  testMatch: ['<rootDir>/test/unit/**/*.test.ts', '<rootDir>/test/unit/**/*.test.js'],
   collectCoverage: true,
   coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}], "json-summary"],
   coverageDirectory: 'coverage',
-  testPathIgnorePatterns: ['<rootDir>/test/integration/', '<rootDir>/test/performance/']
+  collectCoverageFrom: [
+    'src/data-structures/**/*.{ts,js}',
+    'src/types/**/*.{ts,js}',
+    'src/interfaces/**/*.{ts,js}',
+  ],
 };
