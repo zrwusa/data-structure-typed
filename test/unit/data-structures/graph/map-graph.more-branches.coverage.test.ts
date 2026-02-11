346d61e07e18d7d50d6409e62cd8cf0afbc4adb1
Revone <zrwusa@gmail.com>
2026-02-12 00:13:39 +1300

    test(map-graph): cover _createInstance originCoord/bottomRight ?? fallbacks

---
+++ test/unit/data-structures/graph/map-graph.more-branches.coverage.test.ts
@@ -0,0 +1,17 @@
+import { MapGraph } from '../../../../src';
+
+describe('MapGraph remaining branch coverage', () => {
+  it('_createInstance falls back to this.originCoord/this.bottomRight when options omit them (?? rhs)', () => {
+    const g = new MapGraph<number, number>([10, 20], [30, 40]);
+
+    const inst = (g as any)._createInstance({});
+    expect(inst).toBeInstanceOf(MapGraph);
+
+    // Verify fallbacks were used.
+    expect((inst as any).originCoord).toEqual([10, 20]);
+    expect((inst as any).bottomRight).toEqual([30, 40]);
+
+    const inst2 = (g as any)._createInstance({ bottomRight: undefined });
+    expect((inst2 as any).bottomRight).toEqual([30, 40]);
+  });
+});
